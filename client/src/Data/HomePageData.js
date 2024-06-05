03781fd858d1922f1b00f4668e054bd451a69a2f
Sachdarras <sachdarras@gmail.com>
2024-06-05 14:27:45 +0200

    archi home page fichier HomepageDATA

---
+++ client/src/Data/HomePageData.js
@@ -0,0 +1,33 @@
+const HomepageData = [
+  {
+    title: "Vidéos avec compte",
+    description:
+      "Pour profiter des vidéos avec un compte, veuillez vous inscrie sur notre site afin de bénéficier de cet avanatge",
+    url: "https://www.youtube.com/watch?v=3L6dV42kfm8",
+  },
+  {
+    title: "Meilleur du Sport",
+    description:
+      "Retrouver le meilleur du sport sur notre site avec divers vidéos et contenu",
+    url: "https://www.youtube.com/watch?v=of1UYFZw6Ws",
+  },
+  {
+    title: "Meilleur de la tech",
+    description:
+      "Retrouver le meilleur de la Tech sur notre site avec divers vidéos et contenu",
+    url: "https://www.youtube.com/watch?v=SgEE0D78PNI",
+  },
+  {
+    title: "Suggestions",
+    description:
+      "Vous pouvez retrouver des suggestions correspondant à votre recherche sur notre site",
+    url: "https://www.youtube.com/watch?v=j4jfxEEbJMY",
+  },
+  {
+    title: "Recommandations",
+    description:
+      "Vous pouvez retrouver des recomandations correspondant à votre recherche sur notre site",
+    url: "https://www.youtube.com/watch?v=W9izkPFfUeg",
+  },
+];
+export default HomepageData;
