bd6937f02e3efe47f8422dcb01e2d8964c7503fd
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-06-05 22:41:19 +0200

    init header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,50 @@
+// Import react
+import { useState } from "react";
+
+// Import des images
+import LOGO from "../assets/images/origindigital.svg";
+import MENU from "../assets/images/images-header/menu.svg";
+import CROSS from "../assets/images/images-header/cross.svg";
+import videoMenuBurger from "../assets/images/images-header/videoMenuBurger.mov";
+import videohomepage from "../assets/images/images-header/videohomepage.mp4";
+
 function Header() {
-  return <h1>Hello world</h1>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="header">
+      <div className="filter-video" />
+      <video className="video-header" autoPlay muted loop>
+        <source src={videohomepage} type="video/mp4" />
+      </video>
+      <img className="logo" src={LOGO} alt="logo" />
+      <div className={menuOpen ? "sidenav active" : "sidenav"}>
+        <video autoPlay muted loop className="background-video">
+          <source src={videoMenuBurger} type="video/mp4" />
+        </video>
+        <button type="button" className="close" onClick={toggleMenu}>
+          <img className="logo-cross" src={CROSS} alt="fermer" />
+        </button>
+        <ul>
+          <li>Accueil</li>
+          <li>Catégories</li>
+          <li>Sous-Catégories</li>
+          <li>Admin</li>
+          <li>pourquoi pas s'abonner ?</li>
+          <li>se connecter</li>
+        </ul>
+      </div>
+      <button type="button" id="openBtn" onClick={toggleMenu}>
+        <span className="burger-icon">
+          <img className="logo-burger" src={MENU} alt="menu burger" />
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default Header;
