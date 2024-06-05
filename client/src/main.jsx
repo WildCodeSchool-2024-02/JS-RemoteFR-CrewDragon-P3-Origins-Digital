f677076175e86343c1cff772c66bd7de325218e5
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-06-05 10:56:26 +0200

    init front

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,55 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Import du Composant App
 
 import App from "./App";
 
+// Import des pages
+
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import SousCategories from "./pages/SousCategories";
+import Contenue from "./pages/Contenue";
+import Admin from "./pages/Admin";
+import Error404 from "./pages/Error404";
+
+// router creation
+
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Categories",
+        element: <Categories />,
+      },
+      {
+        path: "/SousCategories",
+        element: <SousCategories />,
+      },
+      {
+        path: "/Contenue",
+        element: <Contenue />,
+      },
+      {
+        path: "/Admin",
+        element: <Admin />,
+      },
+      {
+        path: "/error404",
+        element: <Error404 />,
+      },
+    ],
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// rendering
 
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
---
+++ client/src/pages/SousCategories.jsx
@@ -0,0 +1,5 @@
+function SousCategories() {
+  return <h1>Hello world</h1>;
+}
+
+export default SousCategories;
