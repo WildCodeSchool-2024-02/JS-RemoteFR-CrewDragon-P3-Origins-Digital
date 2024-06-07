a32cb3f27923587538afc18bb2363115229a319d
Jeviraj Jeyaratnam <jeviraj.j@gmail.com>
2024-06-07 11:30:58 +0200

    add link to

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,4 +1,5 @@
 import "../style/Categories.scss";
+import { Link } from "react-router-dom";
 
 function Categories() {
   return (
@@ -7,21 +8,29 @@ function Categories() {
         Veuillez choisir une <span>catégorie</span>
       </h1>
       <div className="layout">
-        <div className="image-container">
-          <div className="overlay-text">Sport</div>
-        </div>
+        <Link to="/SousCategories">
+          <div className="image-container">
+            <div className="overlay-text">Sport</div>
+          </div>
+        </Link>
 
-        <div className="image-container2">
-          <div className="overlay-text">Actualité</div>
-        </div>
+        <Link to="/SousCategories">
+          <div className="image-container2">
+            <div className="overlay-text">Actualité</div>
+          </div>
+        </Link>
 
-        <div className="image-container3">
-          <div className="overlay-text">Technologie</div>
-        </div>
+        <Link to="/SousCategories">
+          <div className="image-container3">
+            <div className="overlay-text">Technologie</div>
+          </div>
+        </Link>
 
-        <div className="image-container4">
-          <div className="overlay-text">Junior</div>
-        </div>
+        <Link to="/SousCategories">
+          <div className="image-container4">
+            <div className="overlay-text">Junior</div>
+          </div>
+        </Link>
       </div>
     </>
   );
